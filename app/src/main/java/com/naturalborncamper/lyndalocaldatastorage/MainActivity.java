9518e18cdf74042ae79800d5e9d2bb4cf2928c2b
NaturalBornCamper <marco@naturalborncamper.com>
2017-08-22 17:30:19 +0800

    After video 0.3 =Starting app

---
+++ app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/MainActivity.java
@@ -0,0 +1,20 @@
+package com.naturalborncamper.lyndalocaldatastorage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.out);
+        tvOut.setText("Ready to code!");
+
+    }
+}
