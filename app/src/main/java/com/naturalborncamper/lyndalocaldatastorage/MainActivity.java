3141a85b9f2e848edc516fd2b8b66cb59309df73
NaturalBornCamper <marco@naturalborncamper.com>
2017-08-22 18:46:20 +0800

    After video 1.3 =Adds ListView in activity using the android list_view_1 predefined view =Adds an ArrayAdapter to link the array data to the ListView

--- app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/MainActivity.java
+++ app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/MainActivity.java
@@ -2,11 +2,14 @@ package com.naturalborncamper.lyndalocaldatastorage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.naturalborncamper.lyndalocaldatastorage.model.DataItem;
 import com.naturalborncamper.lyndalocaldatastorage.sample.SampleDataProvider;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvOut;
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
+    List<String> itemNames = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,19 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
 //        DataItem item = new DataItem(null, "My menu item", "a category", "a descrition", 1, 9.95, "apple_pie.jpg");
 
-        tvOut = (TextView) findViewById(R.id.out);
-        tvOut.setText("");
+//        tvOut = (TextView) findViewById(R.id.out);
+//        tvOut.setText("");
 
-        Collections.sort(dataItemList, new Comparator<DataItem>() {
-            @Override
-            public int compare(DataItem o1, DataItem o2) {
-                return o1.getItemName().compareTo(o2.getItemName());
-            }
-        });
+//        Collections.sort(dataItemList, new Comparator<DataItem>() {
+//            @Override
+//            public int compare(DataItem o1, DataItem o2) {
+//                return o1.getItemName().compareTo(o2.getItemName());
+//            }
+//        });
 
         for (DataItem item: dataItemList){
-            tvOut.append(item.getItemName() + "\n");
+//            tvOut.append(item.getItemName() + "\n");
+            itemNames.add(item.getItemName());
         }
+        Collections.sort(itemNames);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, itemNames
+        );
+
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
     }
 }
