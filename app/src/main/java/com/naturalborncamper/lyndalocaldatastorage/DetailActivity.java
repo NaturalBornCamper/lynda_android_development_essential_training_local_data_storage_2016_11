4b3cbdfa56f6030129a63b48317bde754e9f4343
NaturalBornCamper <marco@naturalborncamper.com>
2017-08-23 15:54:24 +0800

    After video 1.10 =Adds plugin Parcelable to Android Studio, that permits to send a complex object as an extra by parsing the object. (Code at the end of DataItem added automatically by intention action)

--- app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/DetailActivity.java
+++ app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/DetailActivity.java
@@ -14,10 +14,12 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        String itemId = getIntent().getExtras().getString(DataItemAdapter.ITEM_ID_KEY);
-        DataItem item = SampleDataProvider.dataItemMap.get(itemId);
-        Toast.makeText(this, "Received item: " + item.getItemName(), Toast.LENGTH_SHORT).show();
-
-
+//        String itemId = getIntent().getExtras().getString(DataItemAdapter.ITEM_ID_KEY);
+//        DataItem item = SampleDataProvider.dataItemMap.get(itemId);
+        DataItem item = getIntent().getExtras().getParcelable(DataItemAdapter.ITEM_KEY);
+        if (item != null)
+            Toast.makeText(this, "Received item: " + item.getItemName(), Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Didn't receive any data", Toast.LENGTH_SHORT).show();
     }
 }
