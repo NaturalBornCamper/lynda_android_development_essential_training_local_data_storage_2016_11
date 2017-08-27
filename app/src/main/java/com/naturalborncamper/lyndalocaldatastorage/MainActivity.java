faaa2e68ac32f64be29d0ed911e670f0f2105173
NaturalBornCamper <marco@naturalborncamper.com>
2017-08-27 16:08:03 +0800

    After video 3.1 & 3.2 =Adds FileActivity where we create/read/delete files on internal and external storage. Note: forgot to commit after first video but it's the same steps except we ask for permission first on External storage (permission code from Author's git) then the read and delete methods have one line different for opening the file and deleting it.

--- app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/MainActivity.java
+++ app/src/main/java/com/naturalborncamper/lyndalocaldatastorage/MainActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -90,4 +93,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void gotoFileActivity(View view) {
+        Intent fileIntent = new Intent(this, FileActivity.class);
+        startActivity(fileIntent);
+    }
 }
